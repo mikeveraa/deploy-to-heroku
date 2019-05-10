cc88ef11cc7fb0f7c801ac07365231fae1716717
VictorBayu <victorbayu85@gmail.com>
2019-05-10 13:35:19 +0700

    update ke heroku

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/deployangul'));
 
 app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/dist/index.html');
+  res.status(200).sendFile(__dirname + '/deployangul/index.html');
 });
 
 app.listen(process.env.PORT || 8080);
